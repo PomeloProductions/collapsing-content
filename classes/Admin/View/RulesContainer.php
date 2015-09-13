6819e70d44b7eabc3568a7a7e43a615dd6702ebd
Bryce Meyer <bryce@axolotlinteractive.com>
2015-09-13 01:47:34 -0500

    added class for rules container

---
+++ classes/Admin/View/RulesContainer.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 9/13/15
+ * Time: 1:25 AM
+ */
+
+namespace RulesRegulations\Admin\View;
+
+
+use RulesRegulations\Model\Rule;
+use WordWrap\LifeCycle;
+use WordWrap\View\ViewCollection;
+
+class RulesContainer extends ViewCollection {
+
+    /**
+     * @param LifeCycle $lifeCycle
+     * @param Rule[] $rules
+     * @param Rule|null $parent the parent Rule
+     */
+    public function __constructor(LifeCycle $lifeCycle, $rules, Rule $parent = null) {
+        parent::__construct($lifeCycle, "admin/rules_container");
+
+        foreach($rules as $rule) {
+            $view = new RuleTR($this->lifeCycle, $rule->id, $rule->title);
+
+            $this->addChildView("rules", $view);
+        }
+
+        if($parent != null)
+            $this->setTemplateVar("parent", "&parent_id=" . $parent->id);
+
+    }
+}
