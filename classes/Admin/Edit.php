727bfd4a13a4ac79440ec9fd839897eeee3620bd
Bryce Meyer <bryce@axolotlinteractive.com>
2015-09-13 04:15:20 -0500

    added edit function

--- classes/Admin/Edit.php
+++ classes/Admin/Edit.php
@@ -9,15 +9,77 @@
 namespace RulesRegulations\Admin;
 
 
+use RulesRegulations\Admin\View\RulesContainer;
+use RulesRegulations\Model\Rule;
 use WordWrap\Admin\TaskController;
+use WordWrap\View\Editor;
+use WordWrap\View\View;
+use WordWrap\View\ViewCollection;
 
 class Edit extends TaskController{
+
+    /**
+     * @var Rule the rule that is currently being edited
+     */
+    private $rule;
+
+    /**
+     * override this to setup anything that needs to be done before
+     */
+    public function setup() {
+        if(!isset($_GET["id"]) || $_GET["id"] == "")
+            wp_redirect("admin.php?page=rules_regulations&task=view");
+
+        $this->rule = Rule::find_one($_GET["id"]);
+
+    }
 
     /**
      * override to render the main page
      */
     public function renderMainContent() {
-        // TODO: Implement renderMainContent() method.
+
+        $view = new View($this->lifeCycle, "admin/rule_edit");
+
+        $view->setTemplateVar("task", $this->task->getSlug());
+
+        $title = "";
+        $id = "";
+        $aboveRules = "";
+        $belowRules = "";
+
+        $childrenRules = [];
+
+        if(isset($this->rule)) {
+            $title = $this->rule->title;
+            $id = "&id=" . $this->rule->id;
+            $aboveRules = $this->rule->top_content;
+            $belowRules = $this->rule->bottom_content;
+
+            $childrenRules = $this->rule->getChildren();
+        }
+
+        if(isset($_POST["title"]))
+            $title = $_POST["title"];
+        if(isset($_POST["above_rules"]))
+            $aboveRules = $_POST["above_rules"];
+        if(isset($_POST["below_rules"]))
+            $belowRules = $_POST["below_rules"];
+
+        $view->setTemplateVar("title", $title);
+        $view->setTemplateVar("id", $id);
+
+        $aboveEditor = new Editor($this->lifeCycle, "above_rules", $aboveRules, "Above Children Rules");
+        $view->setTemplateVar("above_rules", $aboveEditor->export());
+
+        $belowEditor = new Editor($this->lifeCycle, "below_rules", $belowRules, "Below Children Rules");
+        $view->setTemplateVar("below_rules", $belowEditor->export());
+
+        $rulesContainer = new RulesContainer($this->lifeCycle, $childrenRules, $this->rule);
+        $view->setTemplateVar("rules", $rulesContainer->export());
+
+        return $view->export();
+
     }
 
     /**
@@ -25,12 +87,5 @@ class Edit extends TaskController{
      */
     public function renderSidebarContent() {
         // TODO: Implement renderSidebarContent() method.
-    }
-
-    /**
-     * override this to setup anything that needs to be done before
-     */
-    public function setup() {
-        // TODO: Implement setup() method.
     }
 }
