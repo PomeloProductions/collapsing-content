71683bf1ab2c887f774d7381e460d529c24c9655
Bryce Meyer <bryce@axolotlinteractive.com>
2015-09-12 22:59:01 -0500

    added admin tasks

---
+++ classes/AdminTasks/Create.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 9/12/15
+ * Time: 12:57 PM
+ */
+
+namespace RulesRegulations\AdminTasks;
+
+
+use WordWrap\Admin\TaskController;
+
+class Create extends TaskController {
+
+    /**
+     * override to render the main page
+     */
+    public function renderMainContent() {
+        // TODO: Implement renderMainContent() method.
+    }
+
+    /**
+     * override to render the main page
+     */
+    public function renderSidebarContent() {
+        // TODO: Implement renderSidebarContent() method.
+    }
+
+    /**
+     * override this to setup anything that needs to be done before
+     */
+    public function setup() {
+        // TODO: Implement setup() method.
+    }
+}
