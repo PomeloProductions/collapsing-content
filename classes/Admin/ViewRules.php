f5bf0c8790a0045f8037f2bc7876c35cbdc9ecb1
Bryce Meyer <bryce@axolotlinteractive.com>
2015-09-13 00:47:19 -0500

    renamed admin package

---
+++ classes/Admin/ViewRules.php
@@ -0,0 +1,50 @@
+<?php
+namespace RulesRegulations\AdminTasks;
+use RulesRegulations\Model\Rule;
+use WordWrap\Admin\TaskController;
+use WordWrap\View\View;
+use WordWrap\View\ViewCollection;
+
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 9/12/15
+ * Time: 12:47 PM
+ */
+class ViewRules extends TaskController {
+
+    /**
+     * @var Rule[] all top level rules that exist
+     */
+    private $topLevelRules = [];
+
+    /**
+     * override this to setup anything that needs to be done before
+     */
+    public function setup() {
+        $this->topLevelRules = Rule::fetchAllParents();
+    }
+
+    /**
+     * override to render the main page
+     */
+    public function renderMainContent() {
+        $viewCollection = new ViewCollection($this->lifeCycle, "admin/view_container");
+
+        foreach($this->topLevelRules as $rule) {
+            $view = new View($this->lifeCycle, "admin/rule_tr");
+
+            $view->setTemplateVar("name", $rule->title);
+            $view->setTemplateVar("id", $rule->id);
+        }
+    }
+
+    /**
+     * override to render the main page
+     */
+    public function renderSidebarContent() {
+        // TODO: Implement renderSidebarContent() method.
+    }
+
+
+}
