dd1ec85ed01539f970440e9e3774d07772e3739e
Bryce Meyer <bryce13950@gmail.com>
2015-09-17 02:34:50 -0500

    added delete task

---
+++ classes/Admin/Delete.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bryce
+ * Date: 9/17/15
+ * Time: 2:18 AM
+ */
+
+namespace RulesRegulations\Admin;
+
+
+use RulesRegulations\Model\Rule;
+use WordWrap\Admin\TaskController;
+
+class Delete extends TaskController {
+
+    /**
+     * override this to setup anything that needs to be done before
+     * @param $action string the action the user is trying to complete
+     */
+    public function processRequest($action = null) {
+        $id = isset($_GET["id"]) ? $_GET["id"] : false;
+
+        if(!$id)
+            header("Location: admin.php?page=rules_regulations&task=view_rules");
+
+        $rule = Rule::find_one($id);
+
+        $rule->delete();
+
+        if($rule->getParent())
+            header("Location: admin.php?page=rules_regulations&task=edit_rule&id=" . $rule->getParent()->id);
+        else
+            header("Location: admin.php?page=rules_regulations&task=view_rules");
+    }
+
+    /**
+     * override to render the main page
+     */
+    protected function renderMainContent() { }
+
+    /**
+     * override to render the main page
+     */
+    protected function renderSidebarContent() { }
+}
