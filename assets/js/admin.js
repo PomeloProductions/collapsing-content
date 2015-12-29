c556fb5da030658fac1bacedc97a3cb954e7de04
Bryce Meyer <bryce@axolotlinteractive.com>
2015-12-29 00:23:52 -0600

    added post content

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -3,4 +3,15 @@
  */
 jQuery("document").ready(function() {
 
+    jQuery("#using_post-checkbox").change(function() {
+        if (jQuery(this).is(":checked")) {
+            jQuery("#post-select-container").show();
+            jQuery(".edit-above-entries").hide();
+            jQuery(".edit-below-entries").hide()
+        } else {
+            jQuery("#post-select-container").hide();
+            jQuery(".edit-above-entries").show();
+            jQuery(".edit-below-entries").show();
+        }
+    })
 });
