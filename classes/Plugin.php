b161f90bfa39d229ac31d98a970a503430ab276f
Bryce Meyer <bryce13950@gmail.com>
2017-01-17 05:29:13 -0600

    moved short code registration into config

--- classes/Plugin.php
+++ classes/Plugin.php
@@ -7,18 +7,12 @@ use WordWrap\LifeCycle;
 class Plugin extends LifeCycle {
 
 
-    public function getPluginDisplayName() {
-        return 'Collapsing Content';
-    }
-
 
     public function onInitActionsAndFilters() {
 
 
         $this->assetManager->registerAssetType("html", __DIR__ . "/../assets/html/");
 
-        $sc = new ShortCode($this);
-        $sc->register('collapsing_content');
     }
 
 }
